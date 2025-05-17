97afcb729ddb104fce6a51e8449df8ed6f142472
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2025-05-17 14:42:19 +0200

    chore: add some more things for a monorepo setup

--- sites/docs/astro.config.mjs
+++ sites/docs/astro.config.mjs
@@ -1,7 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      {
+        codes: ["en"],
+        path: "en",
+      },
+      {
+        codes: ["nl"],
+        path: "nl",
+      },
+    ],
+  },
+
+  integrations: [svelte(), purgecss()],
 });
